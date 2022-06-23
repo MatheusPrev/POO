cdc49e7fd7bc762a3fe60395208963d35b97b6a1
Matheus Previatti <m.g.previatti@uol.com.br>
2022-06-23 17:55:15 -0300

    primeiro commit

---
+++ src/aula1/Aula1.java
@@ -0,0 +1,11 @@
+package aula1;
+
+public class Aula1 {
+	public static void main(String[] args) {
+		System.out.print("Ola, Matheus Gomes Previatti");
+	}
+	
+	double distancia(double[ ] p1, double[ ] p2) {
+		return p1[0] + p2[0];
+	}
+}
